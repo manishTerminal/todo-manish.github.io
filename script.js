b816dad2ce1b052022859081624b1a6d8310b48f
manishTerminal <iammanish61@gmail.com>
2023-11-19 12:51:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//1. select all the required element
+
+const form = document.querySelector("#form")
+const input = document.querySelector("#item")
+const orderedlist = document.querySelector(".orderedlist")
+
+//2. Add item to the html page
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+
+	const item = document.createElement("li")
+	item.innerText = input.value
+	item.classList.add("list")
+
+	console.log(item)
+
+	orderedlist.appendChild(item)
+
+	input.value = ""
+
+	item.addEventListener("click", () => {
+		item.remove()
+	})
+})
